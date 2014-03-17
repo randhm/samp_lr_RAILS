4d1ad1a342c21766f31b3db101ceae309aade4ed
Mark Randhawa <mark.randhawa@gmail.com>
2014-03-17 13:43:52 +0000

    initial commit; created rails app

---
+++ samp_lr_RAILS_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampLrRailsApp::Application.config.secret_token = '1bfc60ff9ab68a3288d6302c652263bbd4976c8060d093f761182b0aee6a1e3770c494709731b8ae49ca000ad133b3ff66f3d94d7deb42e5c36e6fcf2c1d69a2'
